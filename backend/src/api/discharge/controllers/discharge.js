e0447d57b9a85ae563743b4f23cf0a9889f54800
josue padron <josuempadron23@hotmail.com>
2023-08-02 20:48:17 -0400

    vistas listas solo falta arreglar las relaciones

---
+++ backend/src/api/discharge/controllers/discharge.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * discharge controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::discharge.discharge');
---
+++ backend/src/api/discharge/routes/discharge.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * discharge router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::discharge.discharge');
